5b4ef5ab95350de1a44e3d741ff18c44422e9b6f
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-07 18:08:37 -0300

    feat: update handler structure to separate subpackage

--- router/routes.go
+++ router/routes.go
@@ -4,64 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	movie := router.Group("/api/v1/movies")
 	{
-		movie.GET("/:id", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Movie",
-			})
-		})
-		movie.POST("", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "CREATE Movie",
-			})
-		})
-		movie.PUT("/:id", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "UPDATE Movie",
-			})
-		})
-		movie.DELETE("/:id", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE Movie",
-			})
-		})
-		movie.GET("", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "LIST Movies",
-			})
-		})
+		movie.GET("/:id", handler.GetMovieHandler)
+		movie.POST("", handler.CreateMovieHandler)
+		movie.PUT("/:id", handler.UpdateMovieHandler)
+		movie.DELETE("/:id", handler.DeleteMovieHandler)
+		movie.GET("", handler.ListMoviesHandler)
 	}
 
 	show := router.Group("/api/v1/shows")
 	{
-		show.GET("/:id", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Show",
-			})
-		})
-		show.POST("", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "CREATE Show",
-			})
-		})
-		show.PUT("/:id", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "UPDATE Show",
-			})
-		})
-		show.DELETE("/:id", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE Show",
-			})
-		})
-		show.GET("", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "LIST Shows",
-			})
-		})
+		show.GET("/:id", handler.GetShowHandler)
+		show.POST("", handler.CreateShowHandler)
+		show.PUT("/:id", handler.UpdateShowHandler)
+		show.DELETE("/:id", handler.DeleteShowHandler)
+		show.GET("", handler.ListShowsHandler)
 	}
 }
