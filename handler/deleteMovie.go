291edc39c5e35006831642692bb8d683b6aac066
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-07 22:44:15 -0300

    feat: add deleteMovie and listMovies logic

--- handler/deleteMovie.go
+++ handler/deleteMovie.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/schemas"
 )
 
 func DeleteMovieHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Movie",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+	}
+	
+	movie := schemas.Movie{}
+
+	if err := db.First(&movie, id).Error; err != nil{
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("movie with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&movie).Error; err != nil{
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting movie with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "Delete Movie", movie)
+
 }
--- handler/listMovies.go
+++ handler/listMovies.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/schemas"
 )
 
 func ListMoviesHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "LIST Movies",
-	})
+	movies := []schemas.Movie{}
+
+	if err := db.Find(&movies).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing movies")
+		return
+	}
+
+	sendSuccess(ctx, "Get List Movies", movies)
 }
