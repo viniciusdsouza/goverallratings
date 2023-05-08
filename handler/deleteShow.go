93c20d595192f8de7086b18fe8c74237e1d1b6c2
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-08 13:31:48 -0300

    feat: add delete show handler logic

--- handler/deleteShow.go
+++ handler/deleteShow.go
@@ -1,13 +1,39 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/schemas"
 )
 
+// @BasePath /api/v1
+// @Summary Delete show
+// @Tags Shows
+// @Accept json
+// @Produce json
+// @Success 200 {object} DeleteShowResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /shows/:id [delete]
 func DeleteShowHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Show",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+	}
+
+	show := schemas.Show{}
+
+	if err := db.First(&show, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("show with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&show).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting show with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "Delete Show", show)
 }
