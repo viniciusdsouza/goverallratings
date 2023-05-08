5dfc2c9448195b0e854d5ba3c34646b4069d7d30
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-08 13:31:33 -0300

    feat: add get show handler logic

--- handler/getShow.go
+++ handler/getShow.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/schemas"
 )
 
+// @BasePath /api/v1
+// @Summary Get show
+// @Tags Shows
+// @Accept json
+// @Produce json
+// @Success 200 {object} GetShowResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /shows/:id [get]
 func GetShowHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Show",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+	}
+
+	show := schemas.Show{}
+	if err := db.First(&show, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("show with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "Get Show", show)
 }
