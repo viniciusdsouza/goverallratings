9b17e07ac04febe7cce58edfd066efe2caf298b3
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-08 13:31:19 -0300

    feat: add list show handler logic

--- handler/listShows.go
+++ handler/listShows.go
@@ -4,10 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/schemas"
 )
 
+// @BasePath /api/v1
+// @Summary List shows
+// @Tags Shows
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListShowResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /shows [get]
 func ListShowsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "LIST Shows",
-	})
+	shows := []schemas.Show{}
+
+	if err := db.Find(&shows).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing shows")
+		return
+	}
+
+	sendSuccess(ctx, "Get List Shows", shows)
 }
