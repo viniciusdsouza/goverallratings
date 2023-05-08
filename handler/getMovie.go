48bfceaa0d16fda24001b34ccd48a7f0bb50a697
viniciusdsouza <vinicius.souza@dock.tech>
2023-05-08 10:47:06 -0300

    feat: add getMovie logic

--- handler/getMovie.go
+++ handler/getMovie.go
@@ -1,13 +1,24 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusdsouza/goverallratings/schemas"
 )
 
 func GetMovieHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Movie",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+	}
+
+	movie := schemas.Movie{}
+	if err := db.First(&movie, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("movie with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "Get Movie", movie)
 }
